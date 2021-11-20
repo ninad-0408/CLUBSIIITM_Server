0282991afea6c13af2d3b353a9282e58238245c2
Ninad Kalanke <ninadkalanke@gmail.com>
2021-11-20 15:36:11 +0530

    cors removed

--- App.js
+++ App.js
@@ -16,14 +16,6 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-const corsOptions ={
-	origin:'*', 
-	credentials:true,            
-	optionSuccessStatus:200,
- }
- 
-app.use(cors(corsOptions));
-
 app.use("/club", clubRoute);
 app.use("/event", eventRoute);
 app.use("/approval", approvalRoute);
